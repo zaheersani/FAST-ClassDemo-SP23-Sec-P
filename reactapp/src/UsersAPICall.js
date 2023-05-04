9c418a3a26660c7e441a594902e1ca63295b4fbb
zaheersani <sani.zaheer@gmail.com>
2023-05-04 11:17:20 +0500

    useEffect

--- reactapp/src/UsersAPICall.js
+++ reactapp/src/UsersAPICall.js
@@ -1,21 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default function UsersList() {
 
     const [getUsers, setUsers] = React.useState([])
+    const [getState, setState] =  useState('init state')
+    
+    console.log('Start of Component')
 
-    fetch('https://jsonplaceholder.typicode.com/users')
-        .then(response => response.json())
-        .then(json => {
-            console.log(json)
-            setUsers(json)
-        })
+    useEffect(() => {
+        console.log('Inside useEffect')
+
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(json => {
+                console.log(json)
+                setUsers(JSON.stringify(json))
+            })
+    }, [getUsers])
+
+    console.log('After useEffect')
 
     return (
         <div>
             <h3>List of Users</h3>
+            {getState}
+            <button onClick={() => setState(`updated state ${new Date().getMilliseconds()}`)}>Change State</button>
             <ul>
-                {getUsers.map(u => <li>{u.name}</li>)}
+                {/* {getUsers.map( (u,i) => <li key={i}>{u.name}</li>)} */}
             </ul>
         </div>
     )
--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -12,12 +12,12 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    {/* <BrowserRouter>
       <App />
     </BrowserRouter>
-    {/* <Counter />
-    <Counter2 />
-    <UsersAPICall /> */}
+     <Counter />
+    <Counter2 />*/}
+    <UsersAPICall /> 
   </React.StrictMode>
 );
 
